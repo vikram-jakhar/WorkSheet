11a2a0687d021853bca81c341ea965a63cd64128
vikram-jakhar <vikramjakhar144@gmail.com>
2024-03-27 13:53:41 +0530

    add tab btn

--- src/components/WorkSheet.jsx
+++ src/components/WorkSheet.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import img from '../assets/img/248labs.png'
+import Dompkong from '../assets/img/Dompkong.png'
 import Counter from '../assets/img/counter.png'
 import FT_plan from '../assets/img/ft_plan.png'
 import Design from '../assets/img/desgin.png'
@@ -27,7 +27,7 @@ const WorkSheet = () => {
   const workData = [
     {
       jan: [
-        { id: 1, title: "DompKong", gitHubLink: "https://github.com/vikram-jakhar/DomPKong.git", liveLink: "https://dom-p-kong.vercel.app/", image: (img), date: "03-01 2024", },
+        { id: 1, title: "DompKong", gitHubLink: "https://github.com/vikram-jakhar/DomPKong.git", liveLink: "https://dom-p-kong.vercel.app/", image: (Dompkong), date: "03-01 2024", },
         { id: 2, title: "Reset Counter", gitHubLink: "https://github.com/vikram-jakhar/counter-page-reset.git", liveLink: "https://counterpagerestboutton.netlify.app", image: (Counter), date: "15-09-2023" },
         { id: 3, title: "Aviation Landing", gitHubLink: "https://github.com/vikram-jakhar/Aviation_Landing.git", liveLink: "https://aviation-landing.vercel.app/", image: (FT_plan), date: "10-01-2024" },
         { id: 4, title: "Design", gitHubLink: "https://github.com/vikram-jakhar/Design.git", liveLink: "https://design-seven-mu.vercel.app/", image: (Design), date: "20-01-2024" },
@@ -52,7 +52,7 @@ const WorkSheet = () => {
   ]
   return (
     <>
-      <div className=' min-h-screen'>
+      <>
 
         <div className="container-xl ">
           <div className='grid grid-cols-10 bg-white p-4 flex-col min-h-screen shadow '>
@@ -66,57 +66,58 @@ const WorkSheet = () => {
                 <button onClick={() => tabchange(2)} className={` ${first === 2 && "text-white  !bg-[green]"} max-w-[122px] text-[20px] font-serif font-normal cursor-pointer bg-transparent border-[3px] p-[4px_45px] rounded-[50px] hover:bg-[green] hover:text-[white] inline-block  border-solid border-[green]`}>Mar</button>
               </div>
             </div>
-            <div className=' col-span-7 h-[810px] shadow p-28 rounded-[10%]  overflow-hidden overflow-y-scroll'>
+            <div className=' col-span-7 h-[810px] shadow p-12 rounded-[10%]  overflow-hidden overflow-y-scroll'>
               {
                 first === 0 && <div className="p-4 bg-transparent  shadow-xl rounded-2xl">
-                  {isLoading ? "" : (<h2 className="text-[20px] font-serif font-normal text-black opacity-90">January Topics :-</h2>)}
+                  {isLoading ? "" : (<h2 className="text-[20px] font-serif font-semibold text-black opacity-90">January Topics :-</h2>)}
                   <div className="flex flex-col gap-5 ">
                     {workData[0].jan.map((data, index) => (
                       <div className="flex gap-3 pt-5 items-center " key={index}>
-                        {isLoading ? "" : (<span className=" flex items-center  justify-center text-[20px] border px-1 w-[35px] h-[35px] text-white bg-[green] rounded-full">{data.id}</span>)}
+                        {isLoading ? "" : (<span className=" flex items-center font-mono font-thin justify-center text-[20px] border px-1 w-[35px] h-[35px] text-white bg-[green] rounded-full">{data.id}</span>)}
                         {isLoading ? "" : (<img src={data.image} alt={data.title} className=" rounded-xl max-w-[200px] w-full" width={200} height={118} />)}
                         <div className="flex flex-col max-w-[470px] w-full gap-2">
-                          {isLoading ? "" : (<h3 className="text-xl font-bold capitalize">{data.title}<span className="ml-5 text-sm font-light opacity-90">Date of submisson:- {data.date}</span></h3>)}
-                          {isLoading ? "" : (<a target="_blank" href={data.gitHubLink} className="text-gray-600">GitHub link:- <span className="text-blue-500 ">{data.gitHubLink}</span></a>)}
-                          {isLoading ? "" : (<a target="_blank" href={data.liveLink} className="text-gray-600">Live link:- <span className="text-blue-500">{data.liveLink}</span></a>)}
+                          {isLoading ? "" : (<h3 className="text-xl font-sans font-bold capitalize">{data.title}<span className="ml-5 text-sm font-light opacity-90">Date of submisson:- {data.date}</span></h3>)}
+                          {isLoading ? "" : (<a target="_blank" href={data.gitHubLink} className="text-gray-600 font-serif font-extralight">GitHub link:- <span className="text-blue-500 ">{data.gitHubLink}</span></a>)}
+                          {isLoading ? "" : (<a target="_blank" href={data.liveLink} className="text-gray-600 font-serif font-extralight">Live link:- <span className="text-blue-500">{data.liveLink}</span></a>)}
                         </div>
                       </div>
                     ))
                     }
                   </div>
                 </div>
-              }
+              },
               {
                 first === 1 && <div className="p-4 bg-transparent shadow-xl rounded-2xl">
-                  {isLoading ? "" : (<h2 className="text-[20px] font-serif font-normal text-black opacity-90">Feb Topics :-</h2>)}
+                  {isLoading ? "" : (<h2 className="text-[20px] font-serif font-semibold text-black opacity-90">February Topics :-</h2>)}
                   <div className="flex flex-col gap-5 ">
                     {workData[1].feb.map((data, index) => (
                       <div className="flex gap-3 items-center pt-5 " key={index}>
-                        {isLoading ? "" : (<span className=" flex items-center  justify-center text-[20px] border px-1 w-[35px] h-[35px] text-white bg-[green] rounded-full">{data.id}</span>)}
+                        {isLoading ? "" : (<span className=" flex items-center  font-mono font-thin justify-center text-[20px] border px-1 w-[35px] h-[35px] text-white bg-[green] rounded-full">{data.id}</span>)}
                         {isLoading ? "" : (<img src={data.image} alt={data.title} className=" rounded-xl max-w-[200px] w-full" width={200} height={118} />)}
                         <div className="flex flex-col max-w-[470px] w-full gap-2">
-                          {isLoading ? "" : (<h3 className="text-xl font-bold capitalize">{data.title}<span className="ml-5 text-sm font-light opacity-90">Date of submisson:- {data.date}</span></h3>)}
-                          {isLoading ? "" : (<a target="_blank" href={data.gitHubLink} className="text-gray-600">GitHub link:- <span className="text-blue-500 ">{data.gitHubLink}</span></a>)}
-                          {isLoading ? "" : (<a target="_blank" href={data.liveLink} className="text-gray-600">Live link:- <span className="text-blue-500">{data.liveLink}</span></a>)}
+                          {isLoading ? "" : (<h3 className="text-xl  font-sans font-bold capitalize">{data.title}<span className="ml-5 text-sm font-light opacity-90">Date of submisson:- {data.date}</span></h3>)}
+                          {isLoading ? "" : (<a target="_blank" href={data.gitHubLink} className="text-gray-600 font-serif font-extralight">GitHub link:- <span className="text-blue-500 ">{data.gitHubLink}</span></a>)}
+                          {isLoading ? "" : (<a target="_blank" href={data.liveLink} className="text-gray-600 font-serif font-extralight">Live link:- <span className="text-blue-500">{data.liveLink}</span></a>)}
                         </div>
                       </div>
                     ))
                     }
                   </div>
                 </div>
-              }
+              },
+
               {
                 first === 2 && <div className="p-4 bg-transparent shadow-xl rounded-2xl">
-                  {isLoading ? "" : (<h2 className="text-[20px] font-serif font-normal text-black opacity-90">March Topics :-</h2>)}
+                  {isLoading ? "" : (<h2 className="text-[20px] font-serif font-semibold text-black opacity-90">March Topics :-</h2>)}
                   <div className="flex flex-col gap-5 ">
                     {workData[2].march.map((data, index) => (
                       <div className="flex gap-3 pt-5 items-center  " key={index}>
-                        {isLoading ? "" : (<span className=" flex items-center  justify-center text-[20px] border px-1 w-[35px] h-[35px] text-white bg-[green] rounded-full">{data.id}</span>)}
+                        {isLoading ? "" : (<span className=" flex items-center font-mono font-thin justify-center text-[20px] border px-1 w-[35px] h-[35px] text-white bg-[green] rounded-full">{data.id}</span>)}
                         {isLoading ? "" : (<img src={data.image} alt={data.title} className=" rounded-xl max-w-[200px] w-full" width={200} height={118} />)}
                         <div className="flex flex-col max-w-[470px] w-full gap-2">
-                          {isLoading ? "" : (<h3 className="text-xl font-bold capitalize">{data.title}<span className="ml-5 text-sm font-light opacity-90">Date of submisson:- {data.date}</span></h3>)}
-                          {isLoading ? "" : (<a target="_blank" href={data.gitHubLink} className="text-gray-600">GitHub link:- <span className="text-blue-500 ">{data.gitHubLink}</span></a>)}
-                          {isLoading ? "" : (<a target="_blank" href={data.liveLink} className="text-gray-600">Live link:- <span className="text-blue-500">{data.liveLink}</span></a>)}
+                          {isLoading ? "" : (<h3 className="text-xl font-sans font-bold capitalize">{data.title}<span className="ml-5 text-sm font-light opacity-90">Date of submisson:- {data.date}</span></h3>)}
+                          {isLoading ? "" : (<a target="_blank" href={data.gitHubLink} className="text-gray-600 font-serif font-extralight">GitHub link:- <span className="text-blue-500 ">{data.gitHubLink}</span></a>)}
+                          {isLoading ? "" : (<a target="_blank" href={data.liveLink} className="text-gray-600 font-serif font-extralight">Live link:- <span className="text-blue-500">{data.liveLink}</span></a>)}
                         </div>
                       </div>
                     ))
@@ -126,12 +127,12 @@ const WorkSheet = () => {
               }
 
             </div>
-          </div>
-        </div>
-      </div >
+          </div >
+        </div >
+      </ >
 
     </>
-  )
+  ) 
 }
 
 export default WorkSheet
