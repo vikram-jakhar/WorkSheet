c4289f67316213e63b002f38d5fe1b964e712789
vikram-jakhar <vikramjakhar144@gmail.com>
2024-03-30 11:18:26 +0530

    add swiper

--- src/components/WorkSheet.jsx
+++ src/components/WorkSheet.jsx
@@ -11,6 +11,7 @@ import Nexai from '../assets/img/nexai.png'
 import Cloop from '../assets/img/Cloop.png'
 import Labs from '../assets/img/Labs.png'
 import Worksheet from '../assets/img/workSheet.png'
+import Swiper from '../assets/img/swiper.webp'
 
 
 const WorkSheet = () => {
@@ -46,92 +47,93 @@ const WorkSheet = () => {
       march: [
         { id: 1, title: " Cloop", gitHubLink: "https://github.com/vikram-jakhar/cloop.git", liveLink: "https://cloop-one.vercel.app/", image: (Cloop), date: "07-03-2024", },
         { id: 2, title: "248 Labs", gitHubLink: "https://github.com/vikram-jakhar/248Labs.git", liveLink: "https://248-labs-six.vercel.app/", image: (Labs), date: "21-03-2024" },
-        { id: 3, title: "Work-Sheet", gitHubLink: "", liveLink: "https://work-sheet-taupe.vercel.app/", image: (Worksheet), date: "10-01-2024" },
+        { id: 3, title: "Work-Sheet", gitHubLink: "", liveLink: "https://work-sheet-taupe.vercel.app/", image: (Worksheet), date: "10-03-2024" },
+        { id: 4, title: "Swiper", gitHubLink: "https://github.com/vikram-jakhar/PracticePage.git", liveLink: "https://practice-page-three.vercel.app/", image: (Swiper), date: "29-03-2024" },
       ]
     },
   ]
   return (
-   
-      <>
 
-        <div className="container-xl overflow-y-hidden  ">
-          <div className='grid grid-cols-10 bg-white p-4 flex-col min-h-screen shadow '>
-            <div className="col-span-3 max-w-[450px] bg-white border rounded-lg">
-              <h1 className='text-black mt-6 text-center  font-mono font-medium text-[34px] text-nowrap'>
-                MY WorkSheet
-              </h1>
-              <div className='flex flex-col gap-5  justify-center items-center pt-12  '>
-                <button onClick={() => tabchange(0)} className={`${first === 0 && "text-white !bg-[green]"} max-w-[122px] text-[20px] font-serif font-normal cursor-pointer bg-transparent border-[3px] p-[4px_45px] rounded-[50px] hover:bg-[green] hover:text-[white] inline-block  border-solid border-[green] duration-300 ease-in-out'`}>Jan</button>
-                <button onClick={() => tabchange(1)} className={` ${first === 1 && "text-white !bg-[green]"}  max-w-[122px] text-[20px] font-serif font-normal cursor-pointer bg-transparent border-[3px] p-[4px_45px] rounded-[50px] hover:bg-[green] hover:text-[white] inline-block  border-solid border-[green] duration-300 ease-in-out'`}>Feb</button>
-                <button onClick={() => tabchange(2)} className={` ${first === 2 && "text-white  !bg-[green]"} max-w-[122px] text-[20px] font-serif font-normal cursor-pointer bg-transparent border-[3px] p-[4px_45px] rounded-[50px] hover:bg-[green] hover:text-[white] inline-block  border-solid border-[green] duration-300 ease-in-out'`}>Mar</button>
+    <>
+
+      <div className="container-xl overflow-y-hidden  ">
+        <div className='grid grid-cols-10 bg-white p-4 flex-col min-h-screen shadow '>
+          <div className="col-span-3 max-w-[450px] bg-white border rounded-lg">
+            <h1 className='text-black mt-6 text-center  font-mono font-medium text-[34px] text-nowrap'>
+              MY WorkSheet
+            </h1>
+            <div className='flex flex-col gap-5  justify-center items-center pt-12  '>
+              <button onClick={() => tabchange(0)} className={`${first === 0 && "text-white !bg-[green]"} max-w-[122px] text-[20px] font-serif font-normal cursor-pointer bg-transparent border-[3px] p-[4px_45px] rounded-[50px] hover:bg-[green] hover:text-[white] inline-block  border-solid border-[green] duration-300 ease-in-out'`}>Jan</button>
+              <button onClick={() => tabchange(1)} className={` ${first === 1 && "text-white !bg-[green]"}  max-w-[122px] text-[20px] font-serif font-normal cursor-pointer bg-transparent border-[3px] p-[4px_45px] rounded-[50px] hover:bg-[green] hover:text-[white] inline-block  border-solid border-[green] duration-300 ease-in-out'`}>Feb</button>
+              <button onClick={() => tabchange(2)} className={` ${first === 2 && "text-white  !bg-[green]"} max-w-[122px] text-[20px] font-serif font-normal cursor-pointer bg-transparent border-[3px] p-[4px_45px] rounded-[50px] hover:bg-[green] hover:text-[white] inline-block  border-solid border-[green] duration-300 ease-in-out'`}>Mar</button>
+            </div>
+          </div>
+          <div className=' col-span-7 h-[810px] shadow p-12 rounded-[10%]  overflow-hidden overflow-y-scroll'>
+            {
+              first === 0 && <div className="p-4 bg-transparent  shadow-xl rounded-2xl">
+                {isLoading ? "" : (<h2 className="text-[20px] font-serif font-semibold text-black opacity-90">January Topics :-</h2>)}
+                <div className="flex flex-col gap-5 ">
+                  {workData[0].jan.map((data, index) => (
+                    <div className="flex gap-3 pt-5 items-center " key={index}>
+                      {isLoading ? "" : (<span className=" flex items-center font-mono font-thin justify-center text-[20px] border px-1 w-[35px] h-[35px] text-white bg-[green] rounded-full">{data.id}</span>)}
+                      {isLoading ? "" : (<img src={data.image} alt={data.title} className=" rounded-xl max-w-[200px] w-full" width={200} height={118} />)}
+                      <div className="flex flex-col max-w-[470px] w-full gap-2">
+                        {isLoading ? "" : (<h3 className="text-xl font-sans font-bold capitalize">{data.title}<span className="ml-5 text-sm font-light opacity-90">Date of submisson:- {data.date}</span></h3>)}
+                        {isLoading ? "" : (<p className='text-gray-600 font-serif font-extralight'>GitHubLink:- <span className='text-blue-500 '><a className='underline' target="_blank" href={data.gitHubLink} >Visit</a></span></p>)}
+                        {isLoading ? "" : (<p className='text-gray-600 font-serif font-extralight'>LiveLInk:- <span className='text-blue-500 '><a className='underline' target="_blank" href={data.liveLink} >Visit</a></span></p>)}
+                      </div>
+                    </div>
+                  ))
+                  }
+                </div>
               </div>
-            </div>
-            <div className=' col-span-7 h-[810px] shadow p-12 rounded-[10%]  overflow-hidden overflow-y-scroll'>
-              {
-                first === 0 && <div className="p-4 bg-transparent  shadow-xl rounded-2xl">
-                  {isLoading ? "" : (<h2 className="text-[20px] font-serif font-semibold text-black opacity-90">January Topics :-</h2>)}
-                  <div className="flex flex-col gap-5 ">
-                    {workData[0].jan.map((data, index) => (
-                      <div className="flex gap-3 pt-5 items-center " key={index}>
-                        {isLoading ? "" : (<span className=" flex items-center font-mono font-thin justify-center text-[20px] border px-1 w-[35px] h-[35px] text-white bg-[green] rounded-full">{data.id}</span>)}
-                        {isLoading ? "" : (<img src={data.image} alt={data.title} className=" rounded-xl max-w-[200px] w-full" width={200} height={118} />)}
-                        <div className="flex flex-col max-w-[470px] w-full gap-2">
-                          {isLoading ? "" : (<h3 className="text-xl font-sans font-bold capitalize">{data.title}<span className="ml-5 text-sm font-light opacity-90">Date of submisson:- {data.date}</span></h3>)}
-                          {isLoading ? "" : (<p className='text-gray-600 font-serif font-extralight'>GitHubLink:- <span className='text-blue-500 '><a className='underline' target="_blank" href={data.gitHubLink} >Visit</a></span></p>)}
-                          {isLoading ? "" : (<p className='text-gray-600 font-serif font-extralight'>LiveLInk:- <span className='text-blue-500 '><a className='underline' target="_blank" href={data.liveLink} >Visit</a></span></p>)}
-                        </div>
+            }
+            {
+              first === 1 && <div className="p-4 bg-transparent shadow-xl rounded-2xl">
+                {isLoading ? "" : (<h2 className="text-[20px] font-serif font-semibold text-black opacity-90">February Topics :-</h2>)}
+                <div className="flex flex-col gap-5 ">
+                  {workData[1].feb.map((data, index) => (
+                    <div className="flex gap-3 items-center pt-5 " key={index}>
+                      {isLoading ? "" : (<span className=" flex items-center  font-mono font-thin justify-center text-[20px] border px-1 w-[35px] h-[35px] text-white bg-[green] rounded-full">{data.id}</span>)}
+                      {isLoading ? "" : (<img src={data.image} alt={data.title} className=" rounded-xl max-w-[200px] w-full" width={200} height={118} />)}
+                      <div className="flex flex-col max-w-[470px] w-full gap-2">
+                        {isLoading ? "" : (<h3 className="text-xl  font-sans font-bold capitalize">{data.title}<span className="ml-5 text-sm font-light opacity-90">Date of submisson:- {data.date}</span></h3>)}
+                        {isLoading ? "" : (<p className='text-gray-600 font-serif font-extralight'>GitHubLink:- <span className='text-blue-500 '><a className='underline' target="_blank" href={data.gitHubLink} >Visit</a></span></p>)}
+                        {isLoading ? "" : (<p className='text-gray-600 font-serif font-extralight'>LiveLInk:- <span className='text-blue-500 '><a className='underline' target="_blank" href={data.liveLink} >Visit</a></span></p>)}
                       </div>
-                    ))
-                    }
-                  </div>
+                    </div>
+                  ))
+                  }
                 </div>
-              }
-              {
-                first === 1 && <div className="p-4 bg-transparent shadow-xl rounded-2xl">
-                  {isLoading ? "" : (<h2 className="text-[20px] font-serif font-semibold text-black opacity-90">February Topics :-</h2>)}
-                  <div className="flex flex-col gap-5 ">
-                    {workData[1].feb.map((data, index) => (
-                      <div className="flex gap-3 items-center pt-5 " key={index}>
-                        {isLoading ? "" : (<span className=" flex items-center  font-mono font-thin justify-center text-[20px] border px-1 w-[35px] h-[35px] text-white bg-[green] rounded-full">{data.id}</span>)}
-                        {isLoading ? "" : (<img src={data.image} alt={data.title} className=" rounded-xl max-w-[200px] w-full" width={200} height={118} />)}
-                        <div className="flex flex-col max-w-[470px] w-full gap-2">
-                          {isLoading ? "" : (<h3 className="text-xl  font-sans font-bold capitalize">{data.title}<span className="ml-5 text-sm font-light opacity-90">Date of submisson:- {data.date}</span></h3>)}
-                          {isLoading ? "" : (<p className='text-gray-600 font-serif font-extralight'>GitHubLink:- <span className='text-blue-500 '><a className='underline' target="_blank" href={data.gitHubLink} >Visit</a></span></p>)}
-                          {isLoading ? "" : (<p className='text-gray-600 font-serif font-extralight'>LiveLInk:- <span className='text-blue-500 '><a className='underline' target="_blank" href={data.liveLink} >Visit</a></span></p>)}
-                        </div>
-                      </div>
-                    ))
-                    }
-                  </div>
-                </div>
-              }
+              </div>
+            }
 
-              {
-                first === 2 && <div className="p-4 bg-transparent shadow-xl rounded-2xl">
-                  {isLoading ? "" : (<h2 className="text-[20px] font-serif font-semibold text-black opacity-90">March Topics :-</h2>)}
-                  <div className="flex flex-col gap-5 ">
-                    {workData[2].march.map((data, index) => (
-                      <div className="flex gap-3 pt-5 items-center  " key={index}>
-                        {isLoading ? "" : (<span className=" flex items-center font-mono font-thin justify-center text-[20px] border px-1 w-[35px] h-[35px] text-white bg-[green] rounded-full">{data.id}</span>)}
-                        {isLoading ? "" : (<img src={data.image} alt={data.title} className=" rounded-xl max-w-[200px] w-full" width={200} height={118} />)}
-                        <div className="flex flex-col max-w-[470px] w-full gap-2">
-                          {isLoading ? "" : (<h3 className="text-xl font-sans font-bold capitalize">{data.title}<span className="ml-5 text-sm font-light opacity-90">Date of submisson:- {data.date}</span></h3>)}
-                          {isLoading ? "" : (<p className='text-gray-600 font-serif font-extralight'>GitHubLink:- <span className='text-blue-500 '><a className='underline' target="_blank" href={data.gitHubLink} >Visit</a></span></p>)}
-                          {isLoading ? "" : (<p className='text-gray-600 font-serif font-extralight'>LiveLInk:- <span className='text-blue-500 '><a className='underline' target="_blank" href={data.liveLink} >Visit</a></span></p>)}
-                        </div>
+            {
+              first === 2 && <div className="p-4 bg-transparent shadow-xl rounded-2xl">
+                {isLoading ? "" : (<h2 className="text-[20px] font-serif font-semibold text-black opacity-90">March Topics :-</h2>)}
+                <div className="flex flex-col gap-5 ">
+                  {workData[2].march.map((data, index) => (
+                    <div className="flex gap-3 pt-5 items-center  " key={index}>
+                      {isLoading ? "" : (<span className=" flex items-center font-mono font-thin justify-center text-[20px] border px-1 w-[35px] h-[35px] text-white bg-[green] rounded-full">{data.id}</span>)}
+                      {isLoading ? "" : (<img src={data.image} alt={data.title} className=" rounded-xl max-w-[200px] w-full" width={200} height={118} />)}
+                      <div className="flex flex-col max-w-[470px] w-full gap-2">
+                        {isLoading ? "" : (<h3 className="text-xl font-sans font-bold capitalize">{data.title}<span className="ml-5 text-sm font-light opacity-90">Date of submisson:- {data.date}</span></h3>)}
+                        {isLoading ? "" : (<p className='text-gray-600 font-serif font-extralight'>GitHubLink:- <span className='text-blue-500 '><a className='underline' target="_blank" href={data.gitHubLink} >Visit</a></span></p>)}
+                        {isLoading ? "" : (<p className='text-gray-600 font-serif font-extralight'>LiveLInk:- <span className='text-blue-500 '><a className='underline' target="_blank" href={data.liveLink} >Visit</a></span></p>)}
                       </div>
-                    ))
-                    }
-                  </div>
+                    </div>
+                  ))
+                  }
                 </div>
-              }
+              </div>
+            }
 
-            </div>
-          </div >
+          </div>
         </div >
-      </ >
+      </div >
+    </ >
 
-  
+
   )
 }
 
